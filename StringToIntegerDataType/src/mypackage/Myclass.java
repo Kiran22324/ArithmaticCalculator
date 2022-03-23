29bb8e6b40fb42e75457f209dd3596c029a80841
Kiran Shiragumpi <kshiragumpi@gmail.com>
2022-03-23 09:20:31 +0530

    new commit

---
+++ StringToIntegerDataType/src/mypackage/Myclass.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class Myclass 
+{
+
+	public static void main(String[] args) 
+	{
+		String a="120";
+		
+		//String can be converted into Integer
+		System.out.println(Integer.parseInt(a)+20);
+
+	}
+
+}
